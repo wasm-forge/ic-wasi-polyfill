04e8915277bc6878e6f03e3407c824839b916ebd
wasm-forge <122647775+wasm-forge@users.noreply.github.com>
2025-06-28 17:42:03 +0200

    fmt

--- fuzz-tests/src/bindings/mod.rs
+++ fuzz-tests/src/bindings/mod.rs
@@ -1,5 +1,4 @@
 // THIS IS A GENERATED FILE, DO NOT EDIT!
-
 #![allow(dead_code, unused_imports, non_snake_case)]
 
 type CallMode = ic_test::CallMode;
@@ -9,7 +8,5 @@ type DeployMode = ic_test::DeployMode;
 type Deployer = ic_test::IcpUser;
 type DeployBuilder<C> = ic_test::DeployBuilder<C, Caller>;
 
-
 // candid: test/canisters/fs_tests/src/fs_tests_backend/fs_tests_backend.did
 pub mod fs_tests_backend;
-
