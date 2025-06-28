4934dff99091b5cd832b42f70741259a10d270d3
wasm-forge <122647775+wasm-forge@users.noreply.github.com>
2025-06-28 17:54:26 +0200

    clippy

--- fuzz-tests/src/bindings/fs_tests_backend.rs
+++ fuzz-tests/src/bindings/fs_tests_backend.rs
@@ -121,7 +121,7 @@ pub fn canister_id() -> Option<Principal> {
 pub fn wasm() -> Option<Vec<u8>> {
     let mut path = std::path::PathBuf::new();
     path.push("../target/wasm32-wasip1/release/fs_tests_backend_nowasi.wasm");
-    let wasm = std::fs::read(path.as_path())
-        .unwrap_or_else(|_| panic!("wasm binary not found: {:?}", path));
+    let wasm =
+        std::fs::read(path.as_path()).unwrap_or_else(|_| panic!("wasm binary not found: {path:?}"));
     Some(wasm)
 }
