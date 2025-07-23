3404b11b26627eb8f5de76a5aa2cdff9a782f9f7
wasm-forge <122647775+wasm-forge@users.noreply.github.com>
2025-07-23 12:33:24 +0200

    fix: c_tests

--- test_canisters/c_tests/src/run.sh
+++ test_canisters/c_tests/src/run.sh
@@ -5,13 +5,14 @@ rm *.wasm
 set -e
 
 
-cd ../../..
+cd ../../../ic-wasi-polyfill
 pwd
-cargo build --release --target wasm32-wasi --features "report_wasi_calls"
 
-cd test_canisters/c_tests/src
+cargo build --release --target wasm32-wasip1 --features "report_wasi_calls"
 
-/opt/wasi-sdk/bin/clang++ -mexec-model=reactor -fno-exceptions main.cpp -L../../../../../ic-wasi-polyfill/ic-wasi-polyfill/target/wasm32-wasi/release -lic_wasi_polyfill -o main.wasm
+cd ../test_canisters/c_tests/src
+
+/opt/wasi-sdk/bin/clang++ -mexec-model=reactor -fno-exceptions main.cpp -L../../../../ic-wasi-polyfill/target/wasm32-wasip1/release -lic_wasi_polyfill -o main.wasm
 wasi2ic main.wasm nowasi.wasm
 
 dfx canister create c_tests_backend
